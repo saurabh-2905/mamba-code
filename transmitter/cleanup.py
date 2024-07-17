0c3f4ce9d5b4497625af23644f39dcb295587b33
saurabh-2905 <saurabhband2905@gmail.com>
2024-07-17 11:35:41 +0200

    deal with files not present on flash

--- transmitter/cleanup.py
+++ transmitter/cleanup.py
@@ -3,7 +3,10 @@ import os
 
 def clean(numFiles):
     for i in range(numFiles):
-        os.remove('varlist{}'.format(i))
-        os.remove('trace{}'.format(i))
-        print('Deleted varlist{} and trace{}'.format(i,i))
+        try:
+            os.remove('varlist{}'.format(i))
+            os.remove('trace{}'.format(i))
+            print('Deleted varlist{} and trace{}'.format(i,i))
+        except:
+            print('No varlist{} and trace{}'.format(i,i))
     
