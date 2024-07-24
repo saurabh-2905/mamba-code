6b6c34236dae5100d6a651dab04117877ad2cbab
saurabh-2905 <saurabhband2905@gmail.com>
2024-07-24 16:11:41 +0200

    send noise every 7 or 12 s

--- transmitter/noise_board.py
+++ transmitter/noise_board.py
@@ -6,6 +6,7 @@ import pycom
 import sys
 # import random
 from machine import Pin
+import utime
 
 
 pycom.heartbeat(False)
@@ -19,45 +20,54 @@ ext_button = Pin('P10', mode=Pin.IN, pull=Pin.PULL_UP)
 try:
     while True:
         ################# Auto send data every 2 seconds #################
-        # s.send('Hello World, This is noise packet for Testing')
-        # ### blikn the led for 500 ms
-        # pycom.rgbled(0x002200) # green
-        # time.sleep(0.1)
-        # pycom.rgbled(0x000000)  # off
-        # print('Ping {}'.format(i))
-        # i= i+1
-        # # if i%3 < 3:
-        # #     print('Sending after 4 sec')
-        # #     time.sleep(1)
-        # # elif random.random() < 0.8:
-        # #     print('Sending after 6 sec')
-        # #     time.sleep(3)
-        # # else:
-        # #     print('Sending after 8 sec')
-        # #     time.sleep(5)
+        s.send('Hello World, This is noise packet for Testing')
+        ### blikn the led for 500 ms
+        pycom.rgbled(0x002200) # green
+        time.sleep(0.1)
+        pycom.rgbled(0x000000)  # off
+        print('Ping {}'.format(i))
+        i= i+1
         
-        # time.sleep(2)
-        # pycom.rgbled(0x000022) # blue
-        # time.sleep(0.2)
-        # pycom.rgbled(0x000000)  # off
+        
+        time.sleep(1)
+        pycom.rgbled(0x000022) # blue
+        time.sleep(0.2)
+        pycom.rgbled(0x000000)  # off
+
+        if utime.time() % 2 == 0:
+            print('Sleeping for 7 seconds')
+            time.sleep(7)
+        else:
+            print('Sleeping for 12 seconds')
+            time.sleep(12)
+        print('Ping {}'.format(i))
+        # time.sleep(1)
+        # time.sleep(1)
+        # print('Ping {}'.format(i))
+        # time.sleep(1)
+        # time.sleep(1)
+        # print('Ping {}'.format(i))
+        # time.sleep(1)
+        # time.sleep(1)
+        # print('Ping {}'.format(i))
         # time.sleep(1)
 
-        ################# Manual send data #################
+        # ################# Manual send data #################
 
-        if ext_button() == 0:
-            print('Button pressed, send msg')
-            s.send('Hello World, This is noise packet for Testing')
-            # print('Ping {}'.format(i))
-            pycom.rgbled(0x002200) # green
-            time.sleep(0.1)
-            pycom.rgbled(0x000000)  # off
-            time.sleep(0.2)
-        else:
-            print('Button not pressed')
-            pycom.rgbled(0x000022) # blue
-            time.sleep(0.2)
-            pycom.rgbled(0x000000)  # off
-            time.sleep(1)
+        # if ext_button() == 0:
+        #     print('Button pressed, send msg')
+        #     s.send('Hello World, This is noise packet for Testing')
+        #     # print('Ping {}'.format(i))
+        #     pycom.rgbled(0x002200) # green
+        #     time.sleep(0.1)
+        #     pycom.rgbled(0x000000)  # off
+        #     time.sleep(0.2)
+        # else:
+        #     print('Button not pressed')
+        #     pycom.rgbled(0x000022) # blue
+        #     time.sleep(0.2)
+        #     pycom.rgbled(0x000000)  # off
+        #     time.sleep(1)
 
 except:
     s.close()
